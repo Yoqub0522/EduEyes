5feb9e1da10a2d8662ae201a14157e239380db10
Shohjahon <shohjahonsobirjonov2007@gmail.com>
2025-10-20 22:38:20 +0500

    generate token updated

--- common/utils/generate_token.py
+++ common/utils/generate_token.py
@@ -7,6 +7,7 @@ def generate_token_for_user(user, time=timedelta(days=30)):
     refresh = RefreshToken.for_user(user)
     acces = refresh.access_token
     acces['user_id'] = str(user.id)
+    acces['role'] = str(user.role)
     return {
         "access": str(acces),
         "refresh": str(refresh)
